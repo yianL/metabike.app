9750b3a4b2042b1ba72d07a180caf82fa43b2a56
Andrew Lai <andrew.lai@mulesoft.com>
2021-12-22 11:37:29 -0800

    initial setup

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+
+const port = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+httpServer.listen(port, () => {
+  console.log(
+    `Server listening at 127.0.0.1:${port} (${
+      isProduction ? 'Production' : 'Development'
+    })`
+  );
+});
--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:contact@metabike.app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact
         </a>
       </header>
     </div>
